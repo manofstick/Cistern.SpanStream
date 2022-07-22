9beb1241f09fe528aa3b9f9c6f745bbafa558630
Paul Westcott <p.westcott@gmail.com>
2022-07-22 17:56:55 +1000

    UnsafeSpan

---
+++ Cistern.Spanner/Utils/UnsafeSpan.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Cistern.Spanner.Utils
+{
+    internal unsafe readonly struct UnsafeSpan<T>
+    {
+        readonly byte* _ptr;
+        readonly int _sizeOfT;
+        readonly int _length;
+
+        public UnsafeSpan(void* ptr,int sizeOfT, int length)
+        {
+            _ptr = (byte*)ptr;
+            _sizeOfT = sizeOfT;
+            _length = length;
+        }
+
+        public static UnsafeSpan<T> FromStackAllocatedSpan(in Span<T> span) =>
+            new (Unsafe.AsPointer(ref span.GetPinnableReference()), Unsafe.SizeOf<T>(), span.Length);
+
+        public int Length => _length;
+
+        public ref T this[int i] => ref Unsafe.AsRef<T>(_ptr + i * _sizeOfT);
+    }
+}
